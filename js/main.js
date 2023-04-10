a949e2741f5960b3531d6fb687988d0a935498ee
Kirill <kirya.kirilenko.84@mail.ru>
2023-04-10 20:58:01 +0700

    реализует управление порядком отрисовки изображений и устраняет дребезг

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ import './fullsize-rendering.js';
 import './effects.js';
 import './scale.js';
 import {getData, sendData} from './api.js';
-import {renderingPhotos} from './thumbnail-rendering.js';
-import { showAlert } from './util.js';
+import {renderingPhotos,filterDefaultClick,filterRandomClick,filterDiscussedClick} from './thumbnail-rendering.js';
+import { showAlert, debounce, RERENDER_DELAY} from './util.js';
 import {showSuccessMessage, showErrorMessage} from './message.js';
 import {closeEditingForm, setOnFormSubmit} from './form-validation.js';
 
@@ -20,6 +20,18 @@ setOnFormSubmit(async (data) => {
 try {
   const data = await getData();
   renderingPhotos(data);
+  filterDefaultClick(debounce(
+    () => renderingPhotos(data),
+    RERENDER_DELAY
+  ));
+  filterRandomClick(debounce(
+    () => renderingPhotos(data),
+    RERENDER_DELAY
+  ));
+  filterDiscussedClick(debounce(
+    () => renderingPhotos(data),
+    RERENDER_DELAY
+  ));
 } catch (err) {
 
   showAlert(err);
--- js/util.js
+++ js/util.js
@@ -2,6 +2,7 @@
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
 const TIMEOUT = 3000;
+const RERENDER_DELAY = 500;
 function showAlert (message) {
 
   const alertMessage = document.createElement('div');
@@ -21,4 +22,29 @@ function showAlert (message) {
   }, TIMEOUT);
 }
 
-export {isEscapeKey,showAlert};
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+function debounce (callback, timeoutDelay = 500) {
+  // Используем замыкания, чтобы id таймаута у нас навсегда приклеился
+  // к возвращаемой функции с setTimeout, тогда мы его сможем перезаписывать
+  let timeoutId;
+
+  return (...rest) => {
+    // Перед каждым новым вызовом удаляем предыдущий таймаут,
+    // чтобы они не накапливались
+    clearTimeout(timeoutId);
+
+    // Затем устанавливаем новый таймаут с вызовом колбэка на ту же задержку
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+
+    // Таким образом цикл «поставить таймаут - удалить таймаут» будет выполняться,
+    // пока действие совершается чаще, чем переданная задержка timeoutDelay
+  };
+}
+
+export {isEscapeKey,showAlert,getRandomInteger, debounce, RERENDER_DELAY};
